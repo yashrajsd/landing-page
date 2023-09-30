2052d8900e94a40cd74c8e0e4c6c6895daa519ce
Yashraj Deshmukh <107096154+yashrajsd@users.noreply.github.com>
2023-09-30 21:52:41 +0530

    Update ScrollingText.jsx

--- src/Home Sections/Components/ScrollingText.jsx
+++ src/Home Sections/Components/ScrollingText.jsx
@@ -3,10 +3,11 @@ import Marquee from "react-fast-marquee";
 import StarIcon from '@mui/icons-material/Star';
 const ScrollingText = () => {
   return (
-    <div className='scrolling-text-container bg-[#6507FF] py-[2rem] overflow-hidden text-white text-[3.5rem]'>
+    <div className=' text-[5.5rem]  scrolling-text-container bg-[#6507FF] py-[2rem] overflow-hidden text-white text-[3.5rem]'>
       <Marquee>
       <div>
-      <span><StarIcon/>  AI</span> <span>ART!</span> <span>UNLEASH</span> <span>YOUR</span> <span>IMAGINATION</span> <span>WITH</span> <span>OUR</span> <span>POWERFUL</span> <span>TOOL</span> <span>AND</span> <span>YOUR</span> <span>CREATIVITY</span>
+      
+      <span class='text-center icon '><StarIcon/></span><span class='font-extrabold'>  AI</span> <span class='font-style: italic'>ART!</span> <span class='font-extrabold text-outline stroke'>UNLEASH</span> <span class='font-style: italic'>YOUR</span> <span class='font-extrabold stroke'>IMAGINATION</span> <span class='font-extrabold'>WITH</span> <span class='font-weight: 700'>OUR</span> <span class='font-style: italic'>POWERFUL</span> <span>TOOL</span> <span>AND</span> <span>YOUR</span> <span class='font-extrabold stroke'>CREATIVITY</span>
       </div>
       </Marquee>
     </div>
